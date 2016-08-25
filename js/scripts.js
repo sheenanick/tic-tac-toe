e0e0f69025d49d7b950eff678dbfa7433902d29c
Josh Huffman and Sheena Nickerson <josh@>
2016-08-25 11:21:12 -0700

    only allow space to be marked once and alternate turns once a space is marked

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,27 @@ function Space(x, y, id){
   this.htmlID = id;
   this.markedBy;
 }
-// Space.prototype.markSpace = function(player){
-//   if(!this.markedBy){
-//     this.markedBy = player;
-//   } else {
-//     alert("You can't click here");
-//   }
-// }
+
 function Board(){
-  this.space1 = new Space(1, 3, "#space1");
-  this.space2 = new Space(2, 3, "#space2");
-  this.space3 = new Space(3, 3, "#space3");
-  this.space4 = new Space(1, 2, "#space4");
-  this.space5 = new Space(2, 2, "#space5");
-  this.space6 = new Space(3, 2, "#space6");
-  this.space7 = new Space(1, 1, "#space7");
-  this.space8 = new Space(2, 1, "#space8");
-  this.space9 = new Space(3, 1, "#space9");
+  this.spaceArray = [];
+  this.spaceArray.push(new Space(1, 3, "space1"));
+  this.spaceArray.push(new Space(2, 3, "space2"));
+  this.spaceArray.push(new Space(3, 3, "space3"));
+  this.spaceArray.push(new Space(1, 2, "space4"));
+  this.spaceArray.push(new Space(2, 2, "space5"));
+  this.spaceArray.push(new Space(3, 2, "space6"));
+  this.spaceArray.push(new Space(1, 1, "space7"));
+  this.spaceArray.push(new Space(2, 1, "space8"));
+  this.spaceArray.push(new Space(3, 1, "space9"));
+  console.log(this);
+}
+
+Board.prototype.findById = function(id){
+  for(var i = 0; i < 9; i++){
+    if (this.spaceArray[i].htmlID === id){
+      return this.spaceArray[i];
+    }
+  }
 }
 
 function Game(){
@@ -45,11 +49,15 @@ Game.prototype.changePlayer = function() {
 $(document).ready(function() {
   var game = new Game();
   console.log(game);
-  // game.board.space3.markSpace(game.player1);
-  // console.log(game.board.space3.markedBy.mark);
   $(".spot").click(function(){
-    if(!this.markedBy){
-      $(this).append(game.currentPlayer.mark)
+    var clickedID = $(this).attr('id')
+    console.log(clickedID);
+    game.board.findById(clickedID);
+    console.log(game.board.findById(clickedID).markedBy);
+    if(!(game.board.findById(clickedID).markedBy)){
+      $(this).append(game.currentPlayer.mark);
+      game.board.findById(clickedID).markedBy = game.currentPlayer.mark;
+      game.changePlayer();
     } else {
       alert("You can't click here");
     }
